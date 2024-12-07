d5ee6921677b1b7bbc429ea2533149e6e04652e4
najib <najibbudinabdullah@gmail.com>
2024-12-07 15:07:12 +0700

    another formik method on the create form

--- src/views/user/form/UserForm.js
+++ src/views/user/form/UserForm.js
@@ -1,7 +1,6 @@
 
-import { ErrorMessage, Field, Form, Formik } from "formik"
+import { useFormik } from "formik"
 import { validationSchemaCreate, validationSchemaUpdate } from "./validation"
-// import { useDispatch } from "react-redux"
 import CustomButton from "../../../components/button"
 import { useDispatch, useSelector } from "react-redux"
 import { serviceUsers } from "../../../services/actions/users"
@@ -18,62 +17,91 @@ const UserForm = () => {
 
     const isUpdate = Boolean(user?.data?.id);
 
-    const handleSubmit = async (values, { setSubmitting }) => {
+    const handleSubmit = async () => {
         try {
-            console.log(values)
+            console.log(`request: ${JSON.stringify(formik.values)}`);
             if (isUpdate) {
-                dispatch(service.updateUser(values, navigate));
+                dispatch(service.updateUser(formik.values, navigate));
             } else {
-                dispatch(service.createUser(values, navigate));
+                dispatch(service.createUser(formik.values, navigate));
             }
         } catch (error) {
             console.error("Error:", error);
-        } finally {
-            setSubmitting(false);
         }
     };
 
+    const formik = useFormik({
+        initialValues: {
+            id: user?.data?.id, 
+            name: user?.data?.name, 
+            email: user?.data?.email, 
+            password: null
+        },
+        validationSchema: isUpdate ? validationSchemaUpdate : validationSchemaCreate,
+        onSubmit: handleSubmit
+    });
+
+    const handleForm = (event) => {
+        const {target} = event; 
+        formik.setFieldValue(target.name, target.value);
+    }
+
     return (
-        <Formik
-            initialValues={{ 
-                id: user?.data?.id, 
-                name: user?.data?.name, 
-                email: user?.data?.email, 
-                password: "" 
-            }}
-            validationSchema={isUpdate ? validationSchemaUpdate : validationSchemaCreate}
-            onSubmit={handleSubmit}
-        >
-            {({ isSubmitting }) => (
-                <Form className="container text-start mt-5">
-                    <Field type="hidden"  id="id" name="id" />
+        <form onSubmit={formik.handleSubmit} className="container text-start mt-5">
+            <div className="border p-4 shadow rounded">
+                <h2 className="mb-3 text-center fw-bold">{isUpdate ? "Update" : "Create"}</h2>    
+                <input type="hidden"  id="id" name="id" />
+                <div className="mb-3">
+                    <label className="form-label">Name</label>
+                    <input 
+                        type="text" 
+                        id="name" 
+                        name="name" 
+                        value={formik.values.name} 
+                        className={`form-control ${formik.touched.name && formik.errors.name ? 'is-invalid' : ''}`} 
+                        onChange={handleForm} 
+                     />
+                    {formik.touched.name && formik.errors.name ? (
+                        <label className="invalid-feedback">{formik.errors.name}</label>
+                    ) : null}
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email</label>
+                    <input 
+                        type="email" 
+                        id="email" 
+                        name="email" 
+                        value={formik.values.email} 
+                        className={`form-control ${formik.touched.email && formik.errors.email ? 'is-invalid' : ''}`} 
+                        onChange={handleForm} 
+                    />
+                    {formik.touched.email && formik.errors.email ? (
+                        <div className="invalid-feedback">{formik.errors.email}</div>
+                    ) : null}
+                </div>
+                {
+                    !isUpdate && (
                     <div className="mb-3">
-                        <label className="form-label">Name</label>
-                        <Field type="text" id="name" name="name" className="form-control" />
-                        <ErrorMessage name="name" component="div" style={{ color: "red" }} />
+                        <label htmlFor="password" className="form-label">Password</label>
+                        <input 
+                            type="password" 
+                            id="password" 
+                            name="password" 
+                            className={`form-control ${formik.touched.password && formik.errors.password ? 'is-invalid' : ''}`} 
+                            onChange={handleForm} 
+                        />
+                        {formik.touched.password && formik.errors.password ? (
+                            <div className="invalid-feedback">{formik.errors.password}</div>
+                        ) : null}
                     </div>
-                    <div className="mb-3">
-                        <label htmlFor="email" className="form-label">Email</label>
-                        <Field type="email" id="email" name="email" className="form-control" />
-                        <ErrorMessage name="email" component="div" style={{ color: "red" }} />
-                    </div>
-                    {
-                        !isUpdate && (
-                        <div className="mb-3">
-                            <label htmlFor="password" className="form-label">Password</label>
-                            <Field type="password" id="password" name="password" className="form-control" />
-                            <ErrorMessage name="password" component="div" style={{ color: "red" }} />
-                        </div>
-                        )
-                    }
-                    <CustomButton
-                            text="Submit"
-                            className="btn btn-primary"
-                            isDisable={isSubmitting}
-                    />     
-                </Form>
-            )}
-        </Formik>
+                    )
+                }
+                <CustomButton
+                    text="Submit"
+                    className="btn btn-primary mb-3"
+                /> 
+            </div> 
+        </form>
     )
 
 }
