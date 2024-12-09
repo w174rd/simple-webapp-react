e34be98f671e7d4551d136a528d97fbd180ebefa
najib <najibbudinabdullah@gmail.com>
2024-12-09 09:17:54 +0700

    update email validation on create data

--- src/views/user/form/validation.js
+++ src/views/user/form/validation.js
@@ -4,13 +4,17 @@ export const validationSchemaCreate = Yup.object({
     name: Yup.string()
       .required("Name is required"),
     email: Yup.string()
-      .required("Email is required"),
+      .required("Email is required")
+      .email("Invalid email"),
     password: Yup.string()
       .min(6, "Password must be at least 6 characters")
       .required("Password is required"),
   });
 
   export const validationSchemaUpdate = Yup.object({
-    name: Yup.string().required("Name is required"),
-    email: Yup.string().email("Invalid email address").required("Email is required"),
+    name: Yup.string()
+      .required("Name is required"),
+    email: Yup.string()
+      .required("Email is required")
+      .email("Invalid email"),
 });
